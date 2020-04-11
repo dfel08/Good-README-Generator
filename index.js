e40725a9968f02dbac7512605696c5553b85c247
dfel08 <dfellows68@gmail.com>
2020-04-10 19:25:28 -0700

    Add questions to index.js

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 var fs = require("fs")
 
 const questions = [
-
+    {
+        question1: "What is the project title?",
+        answer1: ""
+    },
+    {
+        question2: "Please write a description.",
+        answer2: ""
+    },
+    {
+        question3: "Enter a table of contents.",
+        answer3: ""
+    },
+    {
+        question4: "Enter installation instructions.",
+        answer4: ""
+    },
+    {
+        question5: "What are the usage limitations?",
+        answer5: ""
+    },
+    {
+        question6: "What license is this project under?",
+        answer6: ""
+    },
+    {
+        question7: "List the contributors, or if none, say 'No Contributors'.",
+        answer7: ""
+    },
+    {
+        question8: "Enter the tests used to ensure functionality.",
+        answer8: ""
+    },
+    {
+        question9: "Enter builder's username for GitHub.",
+        answer9: ""
+    },
 ];
 
+const { BadgeFactory } = require('gh-badges')
 
+const bf = new BadgeFactory()
+
+const format = {
+  text: ['build', 'passed'],
+  color: 'green',
+  template: 'flat',
+}
+
+const createBadge = bf.create(format)
 
 function writeToFile(fileName, data) {
+
 }
 
 function init() {
