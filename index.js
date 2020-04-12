772e9052c5c4d2ed4ba8ce0c6200267fff181002
dfel08 <dfellows68@gmail.com>
2020-04-12 15:46:57 -0700

    Update questions to be accessible

--- index.js
+++ index.js
@@ -1,50 +1,6 @@
-var fs = require("fs")
-
-const data = [
-    {
-        question1: "What is the project title?",
-        answers: "\n"
-    },
-    {
-        question2: "Please write a description.",
-        answers: "\n"
-    },
-    {
-        question3: "Enter a table of contents.",
-        answers: "\n"
-    },
-    {
-        question4: "Enter installation instructions.",
-        answers: "\n"
-    },
-    {
-        question5: "What are the usage limitations?",
-        answers: "\n"
-    },
-    {
-        question6: "What license is this project under?",
-        answers: "\n"
-    },
-    {
-        question7: "List the contributors, or if none, say 'No Contributors'.",
-        answers: "\n"
-    },
-    {
-        question8: "Enter the tests used to ensure functionality.",
-        answers: "\n"
-    },
-    {
-        question9: "Enter builder's username for GitHub.",
-        answers: "\n"
-    },
-];
-for (var i = 0; i < data[i].answers.length; i++) {
-    fs.appendFile("README.md", data.answers[i], function(err) {
-        if (err) throw err;
-        console.log("Saved!")
-    })
-}
-
+var fs = require("fs");
+var inquirer = require("inquirer");
+var axios = require("axios");
 const { BadgeFactory } = require('gh-badges')
 
 const bf = new BadgeFactory()
@@ -56,6 +12,50 @@ const format = {
 }
 
 const createBadge = bf.create(format)
+
+
+const data = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please enter a description for the title."
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "Enter a table of contents.",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Enter installation instructions.",
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "What license is this project under?",
+    },
+    {
+        type: "input",
+        name: "Contributors",
+        message: "List the contributors, or if none, say 'No Contributors'.",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Enter the tests used to ensure functionality, or put 'No Tests'",
+    },
+    {
+        question9: "Enter builder's username for GitHub.",
+        answers: "\n"
+    },
+];
+
 
 function writeToFile(fileName, data) {
 
